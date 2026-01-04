d9a654ebb9acea601d2392c4caa2ac6c6f68254c
Yinan Qin <elysia-best@simplelinux.cn.eu.org>
2026-01-04 10:25:34 +0800

    Add -y flag to apt-get install commands
    Signed-off-by: Yinan Qin <elysia-best@simplelinux.cn.eu.org>

--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install build-essential curl tar pkg-config
+sudo apt-get -y install build-essential curl tar pkg-config
 sudo apt-get -y --force-yes install \
   autoconf \
   automake \
